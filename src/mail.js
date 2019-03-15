1c3a4117373cce4e4333d38cee94cf5602fb74d6
Mia Nordentoft <mia@miestasmia.com>
2019-03-15 15:21:04 +0100

    Add confirmation msg when mail is ready

--- src/mail.js
+++ src/mail.js
@@ -3,4 +3,6 @@ import sendgrid from '@sendgrid/mail';
 export default function init () {
 	AKSO.mail = new sendgrid.MailService();
 	AKSO.mail.setApiKey(AKSO.conf.sendgrid.apiKey);
+
+	AKSO.log.info('Sendgrid mail client ready');
 };
