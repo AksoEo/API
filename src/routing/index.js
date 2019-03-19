26236090e3563558687fbd4dfb8fe36bcd6aa17a
Mia Nordentoft <mia@miestasmia.com>
2019-03-19 14:42:13 +0100

    Remove debugging

--- src/routing/index.js
+++ src/routing/index.js
@@ -8,12 +8,7 @@ export default function init () {
 	const router = new express.Router();
 
 	router.all('/', (req, res, next) => {
-		console.log(req.method);
-		console.log(req.url);
-		console.log(req.body);
-		console.log(req.query);
-
-		res.sendStatus(204);
+		res.sendStatus(204); // todo: remove
 	});
 
 	return router;
