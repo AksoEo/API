b28826464f987ccbc9157cfbd317c2b0df9555dd
Mia Nordentoft <mia@miestasmia.com>
2019-04-11 12:12:24 +0200

    Properly handle undefined when encoding to msgpack

--- src/util.js
+++ src/util.js
@@ -1,11 +1,12 @@
 /**
  * Traverses through an object recursively and calls a replacement function
- * @param  {Object}   obj      The object to iterate over
- * @param  {Function} replacer The function to call, must return the new object or undefined to leave the object as is and stop iteration
- * @param  {boolean}  [toJSON] Whether to call toJSON on each object after the replacer function, default to true
- * @return {Object}            The object with replacements made
+ * @param  {Object}   obj               The object to iterate over
+ * @param  {Function} replacer          The function to call, must return the new object or undefined to leave the object as is and stop iteration
+ * @param  {boolean}  [toJSON]          Whether to call toJSON on each object after the replacer function, default to true
+ * @param  {boolean}  [removeUndefined] Whether to remove undefined attributes
+ * @return {Object} The object with replacements made
  */
-export function replaceObject (obj, replacer, toJSON = true) {
+export function replaceObject (obj, replacer, toJSON = true, removeUndefined = true) {
 	const res = replacer(obj);
 	if (res === undefined) { return obj; }
 	obj = res;
@@ -13,7 +14,9 @@ export function replaceObject (obj, replacer, toJSON = true) {
 	if (Array.isArray(obj)) {
 		return obj.map(x => replaceObject(x, replacer));
 	} else if (typeof obj === 'object' && obj !== null) {
-		const entries = Object.entries(obj).map(([k, v]) => [k, replaceObject(v, replacer)]);
+		const entries = Object.entries(obj)
+			.filter(([k, v]) => !removeUndefined || v !== undefined) // eslint-disable-line no-unused-vars
+			.map(([k, v]) => [k, replaceObject(v, replacer)]);
 		const newObj = {};
 		for (let entry of entries) {
 			newObj[entry[0]] = entry[1];
