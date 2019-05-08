b3b1ecba4cd2398fedcc0eb8ff6e5b252abe76c6
Mia Nordentoft <mia@miestasmia.com>
2019-05-08 10:17:01 +0200

    Disable XSS filter header

--- src/http.js
+++ src/http.js
@@ -80,7 +80,9 @@ export function init () {
 
 			// Add middleware
 			if (AKSO.conf.http.helmet) {
-				app.use(helmet());
+				app.use(helmet({
+					xssFilter: false
+				}));
 			} else {
 				AKSO.log.warn('Running without helmet');
 			}
