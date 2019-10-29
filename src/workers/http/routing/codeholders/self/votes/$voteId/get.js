683ac807210c6ff4619ff1a19540c52a940c8f23
Mia Nordentoft <mia@tejo.org>
2019-10-29 12:33:24 +0100

    Add GET /codeholders/self/votes/{voteId}

---
+++ src/workers/http/routing/codeholders/self/votes/$voteId/get.js
@@ -0,0 +1,32 @@
+import QueryUtil from '../../../../../../../lib/query-util';
+import CodeholderVoteResource from '../../../../../../../lib/resources/codeholder-vote-resource';
+
+import parSchema from '../schema';
+
+const schema = {
+	...parSchema,
+	...{
+		query: 'resource',
+		body: null
+	}
+};
+
+export default {
+	schema: schema,
+
+	run: async function run (req, res) {
+		const query = AKSO.db('votes_voters')
+			.innerJoin('votes', 'voteId', 'id')
+			.where({
+				codeholderId: req.user.user,
+				voteId: req.params.voteId
+			});
+
+		QueryUtil.simpleResource(req, schema, query);
+
+		const row = await query;
+		if (!row) { return res.sendStatus(404); }
+		const obj = new CodeholderVoteResource(row, req, schema);
+		res.sendObj(obj);
+	}
+};
--- src/workers/http/routing/codeholders/self/votes/index.js
+++ src/workers/http/routing/codeholders/self/votes/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 import { bindMethod } from '../../..';
 
+import { init as route$$voteId } from './$voteId';
+
 import method$get from './get';
 
 /**
@@ -10,6 +12,8 @@ import method$get from './get';
  */
 export function init () {
 	const router = new express.Router();
+
+	router.use('/:voteId(\\d+)', route$$voteId());
 
 	bindMethod(router, '/', 'get', method$get);
 
