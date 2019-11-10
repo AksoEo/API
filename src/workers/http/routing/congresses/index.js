3b732f0e6b61f057887a25054f22bacce833db0e
Mia Nordentoft <mia@tejo.org>
2019-11-10 21:17:34 +0100

    Add GET /congresses/{congressId}

--- src/workers/http/routing/congresses/index.js
+++ src/workers/http/routing/congresses/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 import { bindMethod } from '..';
 
+import { init as route$$congressId } from './$congressId';
+
 import method$get from './get';
 import method$post from './post';
 
@@ -11,6 +13,8 @@ import method$post from './post';
  */
 export function init () {
 	const router = new express.Router();
+
+	router.use('/:congressId(\\d+)', route$$congressId());
 
 	bindMethod(router, '/', 'get', method$get);
 	bindMethod(router, '/', 'post', method$post);
