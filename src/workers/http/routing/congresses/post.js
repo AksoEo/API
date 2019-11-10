d4c1d758243227460283d80ea2de98fe2cb0598a
Mia Nordentoft <mia@tejo.org>
2019-11-10 21:05:04 +0100

    Add POST /congresses

---
+++ src/workers/http/routing/congresses/post.js
@@ -0,0 +1,46 @@
+import path from 'path';
+
+import AKSOOrganization from '../../../../lib/enums/akso-organization';
+
+export default {
+	schema: {
+		query: null,
+		body: {
+			type: 'object',
+			properties: {
+				org: {
+					type: 'string',
+					enum: AKSOOrganization.allLower.filter(x => x !== 'akso')
+				},
+				name: {
+					type: 'string',
+					minLength: 1,
+					maxLength: 100,
+					pattern: '^[^\\n]+$'
+				},
+				abbrev: {
+					type: 'string',
+					minLength: 1,
+					maxLength: 15,
+					pattern: '^[^\\n]+$',
+					nullable: true
+				}
+			},
+			required: [
+				'org',
+				'name'
+			],
+			additionalProperties: false
+		}
+	},
+
+	run: async function run (req, res) {
+		const orgPerm = 'congresses.create.' + req.body.org;
+		if (!req.hasPermission(orgPerm)) { return res.sendStatus(403); }
+
+		const id = (await AKSO.db('congresses').insert(req.body))[0];
+
+		res.set('Location', path.join(AKSO.conf.http.path, '/congresses/', id.toString()));
+		res.sendStatus(201);
+	}
+};
