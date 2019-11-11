38f785380077746569f5b660080d49163a222772
Mia Nordentoft <mia@tejo.org>
2019-11-11 13:25:18 +0100

    Add GET /congresses/{congressId}/instances/{instanceId}/location_tags/{tagId}

---
+++ src/workers/http/routing/congresses/$congressId/instances/$instanceId/location_tags/$tagId/get.js
@@ -0,0 +1,41 @@
+import QueryUtil from '../../../../../../../../../lib/query-util';
+import SimpleResource from '../../../../../../../../../lib/resources/simple-resource';
+
+import parSchema from '../schema';
+
+const schema = {
+	...parSchema,
+	...{
+		query: 'resource',
+		body: null
+	}
+};
+
+export default {
+	schema: schema,
+
+	run: async function run (req, res) {
+		// Make sure the user has the necessary perms
+		const orgData = await AKSO.db('congresses')
+			.innerJoin('congresses_instances', 'congressId', 'congresses.id')
+			.where({
+				congressId: req.params.congressId,
+				'congresses_instances.id': req.params.instanceId
+			})
+			.first('org');
+		if (!orgData) { return res.sendStatus(404); }
+		if (!req.hasPermission('congress_instances.read.' + orgData.org)) { return res.sendStatus(403); }
+
+		const query = AKSO.db('congresses_instances_locationTags')
+			.where({
+				congressInstanceId: req.params.instanceId,
+				id: req.params.tagId
+			});
+		QueryUtil.simpleResource(req, schema, query);
+
+		const row = await query;
+		if (!row) { return res.sendStatus(404); }
+		const obj = new SimpleResource(row);
+		res.sendObj(obj);
+	}
+};
--- src/workers/http/routing/congresses/$congressId/instances/$instanceId/location_tags/index.js
+++ src/workers/http/routing/congresses/$congressId/instances/$instanceId/location_tags/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 import { bindMethod } from '../../../../..';
 
+import { init as route$$tagId } from './$tagId';
+
 import method$get from './get';
 import method$post from './post';
 
@@ -11,6 +13,8 @@ import method$post from './post';
  */
 export function init () {
 	const router = new express.Router({ mergeParams: true });
+
+	router.use('/:tagId(\\d+)', route$$tagId());
 
 	bindMethod(router, '/', 'get', method$get);
 	bindMethod(router, '/', 'post', method$post);
