3970e4cbdaa64cb4076e9bd6252fb0b914c62059
Mia Nordentoft <mia@tejo.org>
2019-11-20 15:38:15 +0100

    Add GET /lists/public

--- src/workers/http/routing/lists/index.js
+++ src/workers/http/routing/lists/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import { bindMethod } from 'akso/workers/http/routing';
 
 import { init as route$$listId } from './$listId';
+import { init as route$public } from './public';
 
 import method$get from './get';
 import method$post from './post';
@@ -15,6 +16,7 @@ export function init () {
 	const router = new express.Router();
 
 	router.use('/:listId(\\d+)', route$$listId());
+	router.use('/public', route$public());
 
 	bindMethod(router, '/', 'get', method$get);
 	bindMethod(router, '/', 'post', method$post);
---
+++ src/workers/http/routing/lists/public/get.js
@@ -0,0 +1,20 @@
+import QueryUtil from 'akso/lib/query-util';
+
+import parSchema from './schema';
+
+const schema = {
+	...parSchema,
+	...{
+		query: 'collection',
+		body: null
+	}
+};
+
+export default {
+	schema: schema,
+
+	run: async function run (req, res) {
+		const query = AKSO.db('lists');
+		await QueryUtil.handleCollection({ req, res, schema, query });
+	}
+};
