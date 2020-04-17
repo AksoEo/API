198d3e8986a09d69352f74dcbac1a5e3984ec09f
Mia Nordentoft <mia@tejo.org>
2020-04-17 15:56:44 +0200

    Add tasks

---
+++ src/workers/http/routing/tasks/get.js
@@ -0,0 +1,24 @@
+import AKSOOrganization from 'akso/lib/enums/akso-organization';
+
+export default {
+	schema: {},
+
+	run: async function run (req, res) {
+		const aksopayPaymentIntentOrgs = AKSOOrganization.allLower.filter(x => x !== 'akso')
+			.filter(org => req.hasPermission('pay.payment_intents.read.' + org));
+
+		const tasks = {};
+		if (aksopayPaymentIntentOrgs.length) {
+			const tasksRaw = await AKSO.db('pay_intents')
+				.where('status', 'submitted')
+				.whereIn('org', aksopayPaymentIntentOrgs)
+				.count({
+					submitted: AKSO.db.raw('status = "submitted"'),
+					disputed: AKSO.db.raw('status = "disputed"')
+				});
+			tasks.aksopay = tasksRaw[0];
+		}
+
+		res.sendObj(tasks);
+	}
+};
