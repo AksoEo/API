e6775457a83ca75d241a0718577a6dd70aa483f2
Mia Nordentoft <mia@tejo.org>
2021-03-11 12:55:42 +0100

    Signing out deletes the totp cookie

--- src/workers/http/routing/auth/delete.js
+++ src/workers/http/routing/auth/delete.js
@@ -8,6 +8,7 @@ export default {
 		if (req.user && req.user.isUser()) {
 			req.logOut();
 			req.session = null;
+			res.clearCookie('remember_totp', { path: '/' });
 			res.sendStatus(204);
 		} else {
 			res.sendStatus(404);
