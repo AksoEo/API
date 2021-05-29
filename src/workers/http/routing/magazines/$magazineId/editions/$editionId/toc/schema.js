4cc9118ff81bf564e00e3fadffef24ecf8791d08
Mia Nordentoft <mia@tejo.org>
2021-05-29 13:38:48 +0200

    Attempt to add availableRecitationFormats to magazine edition toc entries

--- src/workers/http/routing/magazines/$magazineId/editions/$editionId/toc/schema.js
+++ src/workers/http/routing/magazines/$magazineId/editions/$editionId/toc/schema.js
@@ -7,6 +7,11 @@ export default {
 		'author': 'fs',
 		'recitationAuthor': 'fs',
 		'text': 's',
-		'highlighted': 'f'
+		'highlighted': 'f',
+		'availableRecitationFormats': ''
+	},
+	fieldAliases: {
+		availableRecitationFormats: () =>
+			AKSO.db.raw('(select group_concat(format) from magazines_editions_toc_recitations where magazines_editions_toc_recitations.tocEntryId = magazines_editions_toc.id)')
 	}
 };
