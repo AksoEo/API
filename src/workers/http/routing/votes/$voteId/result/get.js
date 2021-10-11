86088c77f21142111f5fcad28bcd1d3e5c857e1d
Mia Nordentoft <mia@tejo.org>
2021-10-11 09:11:02 +0200

    Only publish results of votes with publishResults

--- src/workers/http/routing/votes/$voteId/result/get.js
+++ src/workers/http/routing/votes/$voteId/result/get.js
@@ -7,7 +7,10 @@ export default {
 	run: async function run (req, res) {
 		// Make sure the user has the necessary perms
 		const voteData = await AKSO.db('votes')
-			.where('id', req.params.voteId)
+			.where({
+				id: req.params.voteId,
+				publishResults: true
+			})
 			.first('org', 'results');
 		if (!voteData) { return res.sendStatus(404); }
 		if (!req.hasPermission('votes.read.' + voteData.org)) { return res.sendStatus(403); }
