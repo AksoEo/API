d7365bbf28ab640bbe96ac4df14f7a2c37b64f94
Mia Nordentoft <mia@tejo.org>
2022-05-20 12:29:32 +0200

    Add Newsletter#numSubscribers

--- src/workers/http/routing/newsletters/schema.js
+++ src/workers/http/routing/newsletters/schema.js
@@ -6,5 +6,10 @@ export const schema = {
 		'name': 'fs',
 		'description': 's',
 		'public': 'f',
+		'numSubscribers': '',
 	},
+	fieldAliases: {
+		'numSubscribers': () =>
+			AKSO.db.raw('SELECT COUNT(1) FROM newsletters_subscribers WHERE newsletters_subscribers.newsletterId = newsletters.id'),
+	}
 };
