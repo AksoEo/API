001a46362595cc42a917a161847fb90f4db415c2
Mia Nordentoft <mia@tejo.org>
2022-10-04 13:29:31 +0200

    Attempted fix for broken codeholder profile pictures PUT

--- src/workers/http/lib/codeholder-util.js
+++ src/workers/http/lib/codeholder-util.js
@@ -27,7 +27,7 @@ export async function setProfilePicture (codeholderId, tmpFile, mimetype, modBy,
 	let pictures;
 	try {
 		// Crop and scale the picture to all the necessary sizes
-		pictures = await cropImgToSizes(tmpFile.path, profilePictureSizes, true);
+		pictures = await cropImgToSizes(tmpFile, profilePictureSizes, true);
 	} catch (e) {
 		const err = new Error('Unable to load picture');
 		err.statusCode = 400;
