5ba7bb0fa7f8f433cc1e4d63651060e0ab4fda1d
Mia Nordentoft <mia@tejo.org>
2023-03-06 13:46:58 +0100

    Fix broken fulltext search on notes in congress participants

--- db.sql
+++ db.sql
@@ -3385,6 +3385,21 @@ SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = @saved_cs_client;
 
 --
+-- Temporary view structure for view `view_congresses_instances_participants_metadata`
+--
+
+DROP TABLE IF EXISTS `view_congresses_instances_participants_metadata`;
+/*!50001 DROP VIEW IF EXISTS `view_congresses_instances_participants_metadata`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `view_congresses_instances_participants_metadata` AS SELECT 
+ 1 AS `dataId`,
+ 1 AS `amountPaid`,
+ 1 AS `hasPaidMinimum`,
+ 1 AS `isValid`*/;
+SET character_set_client = @saved_cs_client;
+
+--
 -- Temporary view structure for view `view_pay_intents_purposes`
 --
 
@@ -3721,7 +3736,25 @@ USE `akso`;
 /*!50001 SET collation_connection      = utf8mb4_unicode_ci */;
 /*!50001 CREATE ALGORITHM=UNDEFINED */
 /*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
-/*!50001 VIEW `view_congresses_instances_participants` AS select `congresses_instances_participants`.`congressInstanceId` AS `congressInstanceId`,`congresses_instances_participants`.`dataId` AS `dataId`,`congresses_instances_participants`.`codeholderId` AS `codeholderId`,`congresses_instances_participants`.`approved` AS `approved`,`congresses_instances_participants`.`notes` AS `notes`,`congresses_instances_participants`.`sequenceId` AS `sequenceId`,`congresses_instances_participants`.`price` AS `price`,`congresses_instances_participants`.`cancelledTime` AS `cancelledTime`,(select coalesce(sum(`pay_triggerHist`.`amountTriggered`),0) from (`pay_triggerHist` join `view_pay_intents_purposes` on(((`pay_triggerHist`.`paymentIntentId` = `view_pay_intents_purposes`.`paymentIntentId`) and (`pay_triggerHist`.`pos` = `view_pay_intents_purposes`.`pos`)))) where ((`view_pay_intents_purposes`.`trigger_congress_registration_dataId` = `congresses_instances_participants`.`dataId`) and (0 = `view_pay_intents_purposes`.`invalid`))) AS `amountPaid`,if((`congresses_instances_registrationForm`.`price_var` = NULL),NULL,((select `amountPaid`) >= least(`congresses_instances_participants`.`price`,coalesce(`congresses_instances_registrationForm`.`price_minUpfront`,`congresses_instances_participants`.`price`)))) AS `hasPaidMinimum`,if((`congresses_instances_participants`.`cancelledTime` <> NULL),false,if(`congresses_instances_registrationForm`.`manualApproval`,`congresses_instances_participants`.`approved`,((0 <> `congresses_instances_participants`.`approved`) or (0 <> (select coalesce(`hasPaidMinimum`,true)))))) AS `isValid` from (`congresses_instances_participants` join `congresses_instances_registrationForm` on((`congresses_instances_participants`.`congressInstanceId` = `congresses_instances_registrationForm`.`congressInstanceId`))) */;
+/*!50001 VIEW `view_congresses_instances_participants` AS select `p`.`congressInstanceId` AS `congressInstanceId`,`p`.`dataId` AS `dataId`,`p`.`codeholderId` AS `codeholderId`,`p`.`approved` AS `approved`,`p`.`notes` AS `notes`,`p`.`sequenceId` AS `sequenceId`,`p`.`price` AS `price`,`p`.`cancelledTime` AS `cancelledTime`,`meta`.`amountPaid` AS `amountPaid`,`meta`.`hasPaidMinimum` AS `hasPaidMinimum`,`meta`.`isValid` AS `isValid` from (`congresses_instances_participants` `p` join `view_congresses_instances_participants_metadata` `meta` on((`meta`.`dataId` = `p`.`dataId`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `view_congresses_instances_participants_metadata`
+--
+
+/*!50001 DROP VIEW IF EXISTS `view_congresses_instances_participants_metadata`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_unicode_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `view_congresses_instances_participants_metadata` AS select `congresses_instances_participants`.`dataId` AS `dataId`,(select coalesce(sum(`pay_triggerHist`.`amountTriggered`),0) from (`pay_triggerHist` join `view_pay_intents_purposes` on(((`pay_triggerHist`.`paymentIntentId` = `view_pay_intents_purposes`.`paymentIntentId`) and (`pay_triggerHist`.`pos` = `view_pay_intents_purposes`.`pos`)))) where ((`view_pay_intents_purposes`.`trigger_congress_registration_dataId` = `congresses_instances_participants`.`dataId`) and (0 = `view_pay_intents_purposes`.`invalid`))) AS `amountPaid`,if((`congresses_instances_registrationForm`.`price_var` = NULL),NULL,((select `amountPaid`) >= least(`congresses_instances_participants`.`price`,coalesce(`congresses_instances_registrationForm`.`price_minUpfront`,`congresses_instances_participants`.`price`)))) AS `hasPaidMinimum`,if((`congresses_instances_participants`.`cancelledTime` <> NULL),false,if(`congresses_instances_registrationForm`.`manualApproval`,`congresses_instances_participants`.`approved`,((0 <> `congresses_instances_participants`.`approved`) or (0 <> (select coalesce(`hasPaidMinimum`,true)))))) AS `isValid` from (`congresses_instances_participants` join `congresses_instances_registrationForm` on((`congresses_instances_participants`.`congressInstanceId` = `congresses_instances_registrationForm`.`congressInstanceId`))) */;
 /*!50001 SET character_set_client      = @saved_cs_client */;
 /*!50001 SET character_set_results     = @saved_cs_results */;
 /*!50001 SET collation_connection      = @saved_col_connection */;
@@ -3807,4 +3840,4 @@ USE `akso`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-02 18:31:15
+-- Dump completed on 2023-03-06 13:44:51
